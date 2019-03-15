f8818ecdaf846d67e9ce852a34f3b582a9f517a3
Emmanuel Cole <emmanuel.c2011@gmail.com>
2019-03-15 14:44:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,16 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  # .create
+  # instantiates and saves the song, and it returns the new
+  # song that was created
+  
+  def self.create
+    instance = self.new
+    @@all << instance
+    instance
+  end
+  
   def self.all
     @@all
   end
